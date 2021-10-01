2a0aaf0585ed57ca9d94b80758e60079d3598505
Lawrence Lin <80787904+lawwwlin@users.noreply.github.com>
2021-10-01 16:32:03 -0700

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@
 
 Project.create(image: 'https://portfolio-rails-2633.herokuapp.com/assets/project01-0ff155425c5a026839c43ae7c5089f1e.jpg', title: 'project 1', description: 'sample project 1')
 Project.create(image: 'https://portfolio-rails-2633.herokuapp.com/assets/project02-ba5afb4eb817317b6be7ba2948be9fe3.jpg', title: 'project 2', description: 'sample project 2')
-Project.create(image: 'https://portfolio-rails-2633.herokuapp.com/assets/project03-f820dd50db97b4bef375e7645466b30e.jpg', title: 'project 3', description: 'sample project 3')
+Project.create(image: 'https://portfolio-rails-2633.herokuapp.com/assets/project06-ec513d22a13d9c4c5f959882fa9ea7bb.jpg', title: 'project 3', description: 'sample project 3')
 Project.create(image: 'https://portfolio-rails-2633.herokuapp.com/assets/project04-1e66995d7e1fc884b9d84f6d9a2f135a.jpg', title: 'project 4', description: 'sample project 4')
 Project.create(image: 'https://portfolio-rails-2633.herokuapp.com/assets/project05-be5f6a9baca0191c49d4b424555f9287.jpg', title: 'project 5', description: 'sample project 5')
 Project.create(image: 'https://portfolio-rails-2633.herokuapp.com/assets/project06-ec513d22a13d9c4c5f959882fa9ea7bb.jpg', title: 'project 6', description: 'sample project 6')
